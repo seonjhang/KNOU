cce4e6b29c5011dc62cf6294ebada4727ddd47a5
Seon <92963545+seonjhang@users.noreply.github.com>
2023-11-19 15:34:01 +0900

    Add files via upload

---
+++ C++/FirstStep.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+// If a name defined in a specific name space is frequently used, it may be cumbersome to designate a name space each time.
+// If you use the phrase 'using', you don't have to repeat 'std::' in 'std::cout' or 'std::endl' as follows.
+
+int main()
+{
+    cout << "My first C++ Program" << endl;
+    return 0;
+}
