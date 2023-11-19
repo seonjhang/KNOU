bab4f02775a4cea4a522b350ac69609120100a56
Seon <92963545+seonjhang@users.noreply.github.com>
2023-11-19 15:55:24 +0900

    Add files via upload

---
+++ C++/CallByRef.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+const float PI = 3.14159265f;
+struct Circle {
+    float radius, cx, cy;
+};
+
+// Enter data of a circle
+void inputData(Circle &c) // Unlike value calls, reference calls carry the reference, or address, of the real parameter.
+{     
+    cout << "Radius of a circle : ";
+    cin >> c.radius;
+    cout << "Center coordinate(x) : ";
+    cin >> c.cx;
+    cout << "Center coordinate(y) : ";
+    cin >> c.cy;
+}
+
+// Output of data of a circle
+void prData(const Circle &c) {
+    cout << "Radius = " << c.radius << endl;
+    cout << "Center coordinate = (" << c.cx << ", " << c.cy << ")" << endl;
+}
+
+// Calculate the area of a circle
+float area(const Circle &c) {
+    return PI * c.radius * c.radius;
+}
+
+int main() {
+    Circle circle = { 1, 2, 3 };
+    inputData(circle);
+    cout << "Information of the circle" << endl;
+    prData(circle);
+    cout << "Area of a circle = " << area(circle) << endl;
+
+    return 0;
+}
+
